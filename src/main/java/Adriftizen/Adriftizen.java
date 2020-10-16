92fa0feec91a9f0ef20b492475ef2e99fc4cff91
AJ_4real <ajolevano@outlook.com>
2020-10-16 17:42:55 -0400

    Async command now works

--- src/main/java/Adriftizen/Adriftizen.java
+++ src/main/java/Adriftizen/Adriftizen.java
@@ -4,13 +4,36 @@ import Adriftizen.commands.AsyncCommand;
 import Adriftizen.events.ChunkEnterEvent;
 import com.denizenscript.denizen.utilities.DenizenAPI;
 import com.denizenscript.denizencore.events.ScriptEvent;
+import com.denizenscript.denizencore.objects.core.ElementTag;
+import com.denizenscript.denizencore.objects.core.ListTag;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Adriftizen extends JavaPlugin {
 
+    public static Adriftizen instance;
+
     @Override
     public void onEnable() {
+        instance = this;
         ScriptEvent.registerScriptEvent(new ChunkEnterEvent());
         DenizenAPI.getCurrentInstance().getCommandRegistry().registerCommand(AsyncCommand.class);
+
+        // <--[tag]
+        // @attribute <ElementTag.urls>
+        // @returns ListTag
+        // @plugin Adriftizen
+        // @description
+        // Returns a list of URLs in the element.
+        // Returns empty list if no URLs are found.
+        // -->
+        ElementTag.registerTag("urls", (attribute, object) -> {
+            ListTag list = new ListTag();
+            for (String s : object.asString().split("\\s+")) {
+                if (s.matches("(https?://)[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)")) {
+                    list.addObject(new ElementTag(s));
+                }
+            }
+            return list;
+        });
     }
 }
--- src/main/java/Adriftizen/commands/AsyncCommand.java
+++ src/main/java/Adriftizen/commands/AsyncCommand.java
@@ -1,14 +1,26 @@
 package Adriftizen.commands;
 
-import java.util.List;
-import java.util.Map.Entry;
+import Adriftizen.Adriftizen;
+import com.denizenscript.denizen.objects.NPCTag;
+import com.denizenscript.denizen.objects.PlayerTag;
+import com.denizenscript.denizen.utilities.debugging.Debug;
 import com.denizenscript.denizen.utilities.implementation.BukkitScriptEntryData;
 import com.denizenscript.denizencore.exceptions.InvalidArgumentsException;
+import com.denizenscript.denizencore.objects.Argument;
 import com.denizenscript.denizencore.objects.ObjectTag;
+import com.denizenscript.denizencore.scripts.ScriptBuilder;
 import com.denizenscript.denizencore.scripts.ScriptEntry;
+import com.denizenscript.denizencore.scripts.ScriptEntryData;
 import com.denizenscript.denizencore.scripts.commands.BracedCommand;
 import com.denizenscript.denizencore.scripts.queues.ScriptQueue;
 import com.denizenscript.denizencore.scripts.queues.core.InstantQueue;
+import com.denizenscript.denizencore.utilities.CoreUtilities;
+import org.bukkit.Bukkit;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AsyncCommand extends BracedCommand {
 
@@ -17,35 +29,113 @@ public class AsyncCommand extends BracedCommand {
 		setSyntax("async");
 		setRequiredArguments(0, 0);
 	}
-
+	boolean hasDefinitions = true;
 	@Override
 	public void parseArgs(ScriptEntry e) throws InvalidArgumentsException {
+		for (Argument arg : e.getProcessedArgs()) {
+			if (!e.hasObject("player")
+					&& arg.matchesPrefix("player")) {
+				e.addObject("player", PlayerTag.valueOf(arg.getValue(), null));
+			}
+			else if (!e.hasObject("npc")
+					&& arg.matchesPrefix("npc")) {
+				e.addObject("npc", NPCTag.valueOf(arg.getValue(), null));
+			}
+			else if (arg.matches("no_definitions")) {
+				hasDefinitions = false;
+			}
+		}
 	}
 
 	@Override
 	public void execute(ScriptEntry e) {
-		ScriptQueue queue = (new InstantQueue(e.getScript().getName()));
-		List<ScriptEntry> cmds = e.getBracedSet().get(0).value;
-		queue.addEntries(cmds);
-        for (Entry<String,ObjectTag> entry : e.getResidingQueue().getAllDefinitions().entrySet()) {
-        	queue.addDefinition(entry.getKey(), entry.getValue());
-        }
-        if (e.entryData instanceof BukkitScriptEntryData) {
-            BukkitScriptEntryData data = (BukkitScriptEntryData) e.entryData;
-            if (data.hasPlayer()) {
-            	for (ScriptEntry en : queue.script_entries) {
-            		((BukkitScriptEntryData)en.entryData).setPlayer(data.getPlayer());
-            	}
-            }
-            if (data.hasNPC()) {
-            	for (ScriptEntry en : queue.script_entries) {
-            		((BukkitScriptEntryData)en.entryData).setNPC(data.getNPC());
-            	}
-            }
-        }
-        queue.contextSource = e.getResidingQueue().contextSource;
-        queue.start();
-        e.setFinished(true);
+		List<String> commands = new ArrayList<>();
+		for (ScriptEntry entry : e.getBracedSet().get(0).value) {
+			commands.add(stringify(entry));
+		}
+		Map<String,String> defs = new HashMap<>();
+		for (Map.Entry<String, ObjectTag> def : e.getResidingQueue().getAllDefinitions().entrySet()) {
+			defs.put(def.getKey(), def.getValue().toString());
+		}
+		Bukkit.getScheduler().scheduleSyncDelayedTask(Adriftizen.instance, new Runnable() {
+			@Override
+			public void run() {
+				List<Object> rawEntries = new ArrayList<>();
+				for (String cmd : commands) {
+					if (cmd.length() > 0) {
+						rawEntries.add(parseCommandsBack(unescape(cmd)));
+					}
+				}
+				ScriptEntryData data = new BukkitScriptEntryData(e.hasObject("player") ? e.getObjectTag("player") : null, e.hasObject("npc") ? e.getObjectTag("npc") : null);
+				List<ScriptEntry> entries = ScriptBuilder.buildScriptEntries(rawEntries, e.getScript().getContainer(), data);
+				if (entries.isEmpty()) {
+					return;
+				}
+				if (e.shouldDebug()) {
+					for (ScriptEntry entry : entries) {
+						entry.shouldDebugBool = false;
+					}
+				}
+				ScriptQueue queue = new InstantQueue("ASYNC_").addEntries(entries);
+				for (Map.Entry<String,String> entry : defs.entrySet()) {
+					queue.addDefinition(entry.getKey(), entry.getValue());
+					Debug.echoDebug(entries.get(0), "Adding definition '" + entry.getKey() + "' as " + entry.getValue());
+				}
+				queue.start();
+			}
+		});
 	}
 
+	public static String escape(String text) {
+		return text.replace("\\", "\\\\").replace("\n", "\\n").replace("\r", "\\r");
+	}
+
+	public static String unescape(String text) {
+		return text.replace("\\n", "\n").replace("\\r", "\r").replace("\\\\", "\\");
+	}
+
+	public static Object parseCommandsBack(String value) {
+		List<String> cmds = CoreUtilities.split(value, '\r');
+		if (cmds.size() == 1) {
+			return cmds.get(0);
+		}
+		List<Object> resultSubList = new ArrayList<>();
+		for (int i = 1; i < cmds.size(); i++) {
+			if (cmds.get(i).length() > 0) {
+				resultSubList.add(parseCommandsBack(unescape(cmds.get(i))));
+			}
+		}
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put(cmds.get(0), resultSubList);
+		return resultMap;
+	}
+
+	public static String stringify(Object obj) {
+		if (obj instanceof ScriptEntry) {
+			ScriptEntry entry = (ScriptEntry) obj;
+			if (entry.internal.insideList == null) {
+				return escape(entry.internal.originalLine);
+			}
+			StringBuilder result = new StringBuilder();
+			result.append(escape(entry.internal.originalLine)).append("\r");
+			for (Object subCommand : entry.internal.insideList) {
+				result.append(stringify(subCommand)).append("\r");
+			}
+			return result.toString();
+		}
+		else if (obj instanceof Map) {
+			Map<Object, Object> valueMap = (Map<Object, Object>) obj;
+			StringBuilder result = new StringBuilder();
+			Object cmdLine = valueMap.keySet().toArray()[0];
+			result.append(escape(cmdLine.toString())).append("\r");
+			List<Object> inside = (List<Object>) valueMap.get(cmdLine);
+			for (Object subCommand : inside) {
+				result.append(stringify(subCommand)).append("\r");
+			}
+			return result.toString();
+		}
+		else {
+			return escape(obj.toString());
+		}
+	}
 }
